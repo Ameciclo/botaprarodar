3b556ee165539749baa34f97d364c41d025e0772
Igor Matos <igormspc@gmail.com>
2019-08-06 17:11:06 -0300

    extract take picture logic

---
+++ app/src/main/java/app/igormatos/botaprarodar/util/ActivityExtensions.kt
@@ -0,0 +1,39 @@
+package app.igormatos.botaprarodar.util
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+
+const val REQUEST_PHOTO = 1
+const val EXTRA_IMAGE_PATH = "EXTRA_IMAGE_PATH"
+
+fun Activity.takePictureIntent(block: (String) -> Unit) {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+        // Ensure that there's a camera activity to handle the intent
+        takePictureIntent.resolveActivity(packageManager)?.also {
+            // Create the File where the photo should go
+            val photoFile: File? = try {
+                createImageFile()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+                null
+            }
+            // Continue only if the File was successfully created
+            photoFile?.also {
+                block(it.absolutePath)
+
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    this,
+                    "app.igormatos.botaprarodar.provider",
+                    it
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_PHOTO)
+            }
+        }
+    }
+}
