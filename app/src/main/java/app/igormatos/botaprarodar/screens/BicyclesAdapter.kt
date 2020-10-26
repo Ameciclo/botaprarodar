eaaf56b6d6fb49105d12a6e7ee0e92e0d0cb05cf
Igor <igormspc@gmail.com>
2020-10-26 19:24:50 -0300

    Refactor: extract bicycle adapter

---
+++ app/src/main/java/app/igormatos/botaprarodar/screens/BicyclesAdapter.kt
@@ -0,0 +1,101 @@
+package app.igormatos.botaprarodar.screens
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import app.igormatos.botaprarodar.R
+import app.igormatos.botaprarodar.data.model.Bicycle
+import com.bumptech.glide.Glide
+
+class BicyclesAdapter(
+    private val bicycleAdapterListener: BicycleAdapterListener
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), BicycleAdapterListener {
+
+    var itemsList: MutableList<Bicycle> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.bicycle_cell, parent, false)
+        return BicycleViewHolder(bicycleAdapterListener, view)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return 0
+    }
+
+    override fun getItemCount(): Int {
+        return itemsList.count()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, index: Int) {
+        val item = itemsList[index]
+        (holder as BicycleViewHolder).bind(item)
+    }
+
+    fun addItem(item: Bicycle) {
+        itemsList.add(0, item)
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(item: Bicycle) {
+        val index = itemsList.indexOfFirst { it.id == item.id }
+        itemsList.removeAt(index)
+        notifyItemRemoved(index)
+    }
+
+    fun updateItem(item: Bicycle) {
+        val index = itemsList.indexOfFirst { it.id == item.id }
+        itemsList[index] = item
+        notifyItemChanged(index)
+    }
+
+    class BicycleViewHolder(
+        private val bicycleAdapterListener: BicycleAdapterListener,
+        itemView: View
+    ) :
+        RecyclerView.ViewHolder(itemView) {
+
+        fun bind(bicycle: Bicycle) {
+            itemView.findViewById<TextView>(R.id.cellTitle).text = bicycle.title()
+            itemView.findViewById<TextView>(R.id.cellSubtitle).text = bicycle.subtitle()
+            val imageView = itemView.findViewById<ImageView>(R.id.cellImageView)
+
+            itemView.setOnLongClickListener {
+                bicycleAdapterListener.onBicycleLongClicked(bicycle)
+                true
+            }
+
+            itemView.setOnClickListener {
+                bicycleAdapterListener.onBicycleClicked(bicycle)
+            }
+
+            if (!bicycle.isAvailable) {
+                Glide.with(itemView.context)
+                    .load(bicycle.iconPath())
+                    .into(imageView)
+
+                val colorMatrix = ColorMatrix()
+                colorMatrix.setSaturation(0.0f)
+                val filter = ColorMatrixColorFilter(colorMatrix)
+                imageView.colorFilter = filter
+
+            } else {
+                Glide.with(itemView.context)
+                    .load(bicycle.iconPath())
+                    .into(imageView)
+            }
+        }
+    }
+
+    override fun onBicycleClicked(bicycle: Bicycle) {
+        bicycleAdapterListener.onBicycleClicked(bicycle)
+    }
+
+    override fun onBicycleLongClicked(bicycle: Bicycle) {
+        bicycleAdapterListener.onBicycleLongClicked(bicycle)
+    }
+}
--- app/src/main/java/app/igormatos/botaprarodar/screens/main/BicyclesFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/screens/main/BicyclesFragment.kt
@@ -5,20 +5,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import app.igormatos.botaprarodar.R
-import app.igormatos.botaprarodar.local.Preferences
 import app.igormatos.botaprarodar.data.model.Bicycle
+import app.igormatos.botaprarodar.local.Preferences
 import app.igormatos.botaprarodar.network.FirebaseHelper
 import app.igormatos.botaprarodar.network.RequestListener
+import app.igormatos.botaprarodar.screens.BicycleAdapterListener
+import app.igormatos.botaprarodar.screens.BicyclesAdapter
 import app.igormatos.botaprarodar.screens.addbicycle.AddBikeActivity
-import app.igormatos.botaprarodar.screens.ItemAdapter
 import kotlinx.android.synthetic.main.fragment_list.*
 
-class BicyclesFragment : androidx.fragment.app.Fragment() {
+class BicyclesFragment : Fragment(), BicycleAdapterListener {
 
-    lateinit var itemAdapter: ItemAdapter
+    lateinit var bicycleAdapter: BicyclesAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val rootView = inflater.inflate(R.layout.fragment_list, container, false)
         return rootView
     }
@@ -26,8 +33,7 @@ class BicyclesFragment : androidx.fragment.app.Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        itemAdapter =
-            ItemAdapter(activity = activity)
+        bicycleAdapter = BicyclesAdapter(bicycleAdapterListener = this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,23 +45,30 @@ class BicyclesFragment : androidx.fragment.app.Fragment() {
         }
 
         recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
-        recyclerView.adapter = itemAdapter
+        recyclerView.adapter = bicycleAdapter
 
         val joinedCommunityId = Preferences.getJoinedCommunity(context!!).id!!
         FirebaseHelper.getBicycles(joinedCommunityId, listener = object : RequestListener<Bicycle> {
             override fun onChildChanged(result: Bicycle) {
-                itemAdapter.updateItem(result)
+                bicycleAdapter.updateItem(result)
             }
 
             override fun onChildAdded(result: Bicycle) {
-                itemAdapter.addItem(result)
+                bicycleAdapter.addItem(result)
             }
 
             override fun onChildRemoved(result: Bicycle) {
-                itemAdapter.removeItem(result)
-
+                bicycleAdapter.removeItem(result)
             }
 
         })
+    }
+
+    override fun onBicycleClicked(bicycle: Bicycle) {
+        Toast.makeText(context!!, bicycle.name, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBicycleLongClicked(bicycle: Bicycle) {
+        Toast.makeText(context!!, bicycle.name, Toast.LENGTH_SHORT).show()
     }
 }
