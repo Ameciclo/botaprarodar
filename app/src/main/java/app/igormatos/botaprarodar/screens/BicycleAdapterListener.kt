6f8a628ff37ebc5af82782f4edf984b1b912134d
Igor <igormspc@gmail.com>
2020-10-27 15:38:06 -0300

    BycycleLongClicked return boolean

--- app/src/main/java/app/igormatos/botaprarodar/screens/BicycleAdapterListener.kt
+++ app/src/main/java/app/igormatos/botaprarodar/screens/BicycleAdapterListener.kt
@@ -4,5 +4,5 @@ import app.igormatos.botaprarodar.data.model.Bicycle
 
 interface BicycleAdapterListener {
     fun onBicycleClicked(bicycle: Bicycle)
-    fun onBicycleLongClicked(bicycle: Bicycle)
+    fun onBicycleLongClicked(bicycle: Bicycle) : Boolean
 }
