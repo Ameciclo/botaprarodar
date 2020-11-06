22dbbbae9bce447cedf2c765d980251a6f7ade3a
Akanbi Strossi <akanbi87@gmail.com>
2020-11-06 19:03:28 -0300

    Add login tests

---
+++ app/src/androidTest/java/app/igormatos/botaprarodar/login/LoginRobot.kt
@@ -0,0 +1,55 @@
+package app.igormatos.botaprarodar.login
+
+import androidx.test.espresso.NoMatchingViewException
+import app.igormatos.botaprarodar.BaseRobot
+import app.igormatos.botaprarodar.R
+
+fun login(executeFun: LoginRobot.() -> Unit) = LoginRobot().apply{ executeFun() }
+
+class LoginRobot : BaseRobot() {
+
+    fun clickLogin() {
+        clickButton(R.id.loginButton)
+    }
+
+    fun clickSignIn() {
+        clickButtonByText("SIGN IN")
+    }
+
+    fun clickNext() {
+        clickButtonByText("Next")
+    }
+
+    fun clickRecoveryPassword() {
+        clickButtonByText("Trouble signing in?")
+    }
+
+    fun fillUserField(user: String) {
+        fillFieldByHint("Email", user)
+    }
+
+    fun fillPasswordField(password: String) {
+        fillFieldByHint("Password", password)
+    }
+
+    fun doLogin(user: String, password: String) {
+        clickLogin()
+        fillUserField(user)
+        clickNext()
+        fillPasswordField(password)
+        clickSignIn()
+    }
+
+    infix fun verify(executeFun: LoginRobot.() -> Unit) {
+        executeFun()
+    }
+
+    fun checkMessageIncorrectPassword() {
+        try {
+            checkMessage("Incorrect password.")
+        } catch (e: NoMatchingViewException) {
+            checkMessage("An unknown error occurred.")
+        }
+    }
+
+}
