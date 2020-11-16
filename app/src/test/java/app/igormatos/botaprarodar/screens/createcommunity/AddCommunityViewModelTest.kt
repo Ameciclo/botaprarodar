01750f00bdb264c7637b2bd9f8ffc04a2877c12c
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2020-11-16 18:19:50 -0300

    Choose community dialog refactor

--- app/src/test/java/app/igormatos/botaprarodar/screens/createcommunity/AddCommunityViewModelTest.kt
+++ app/src/test/java/app/igormatos/botaprarodar/screens/createcommunity/AddCommunityViewModelTest.kt
@@ -1,55 +1,25 @@
 package app.igormatos.botaprarodar.screens.createcommunity
 
-import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import app.igormatos.botaprarodar.network.Community
 import app.igormatos.botaprarodar.network.FirebaseHelperModule
-import com.google.firebase.FirebaseApp
-import junit.framework.Assert.assertNotNull
-import junit.framework.Assert.assertTrue
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.MockitoAnnotations
-import org.mockito.invocation.InvocationOnMock
-import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.stubbing.Answer
 
-@RunWith(MockitoJUnitRunner::class)
+
 class AddCommunityViewModelTest {
+
+    val firebaseHelperModuleMock = mockk<FirebaseHelperModule>(relaxed = true)
+    lateinit var viewModel: AddCommunityViewModel
 
     @get:Rule
     var instantExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Mock
-    lateinit var observer: Observer<Community>
-
-    @Mock
-    lateinit var context: Context
-
-    lateinit var viewModelTest: AddCommunityViewModel
-
     @Before
-    fun setUo() {
-        MockitoAnnotations.initMocks(this)
-
-        viewModelTest.communityData.value = Community()
+    fun setUp() {
+        viewModel = AddCommunityViewModel(firebaseHelperModuleMock)
     }
 
-
-    @Test
-    fun getCommunityFromInputs_shouldChangeLiveDataWithCommunity_shouldBeObserved() {
-        `when`(viewModelTest.getCommunityFromInputs()).then { true }
-        assertNotNull(viewModelTest.communityData)
-        assertTrue(viewModelTest.communityData.hasObservers())
-    }
 }
