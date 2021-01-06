6f8eafb731025b1f7d75feb57f5f7e1b00b4f80c
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-01-06 17:18:14 -0300

    remove generic Status.kt

--- app/src/main/java/app/igormatos/botaprarodar/common/Status.kt
+++
@@ -1,7 +0,0 @@
-package app.igormatos.botaprarodar.common
-
-sealed class Status<T> (val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Status<T>(data)
-    class Loading<T>(data: T?) : Status<T>(data)
-    class Error<T>(message: String?, data: T? = null) : Status<T>(data, message)
-}
