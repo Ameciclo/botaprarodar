00d5b12abc8b7eb93ccb87a4efb05bc58ac3baed
Akme-re <akmere.almeida@thoughtworks.com>
2021-01-07 14:23:02 -0300

    add view binding

--- app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/SignInFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/SignInFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import app.igormatos.botaprarodar.R
+import app.igormatos.botaprarodar.databinding.FragmentSignInBinding
 
 
 class SignInFragment: Fragment() {
+    private lateinit var binding : FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        binding = FragmentSignInBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.next.setOnClickListener {
+            //TODO implement SignIn action
+        }
     }
 }
