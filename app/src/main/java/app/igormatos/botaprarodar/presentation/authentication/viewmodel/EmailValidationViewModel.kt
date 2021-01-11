fa5bd6997c58f8d9893fd7784573ed587566c6c1
Akme-re <akmere.almeida@thoughtworks.com>
2021-01-11 18:24:26 -0300

    add EmailValidationViewModel implementation

--- app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/viewmodel/EmailValidationViewModel.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/viewmodel/EmailValidationViewModel.kt
@@ -1,14 +1,58 @@
 package app.igormatos.botaprarodar.presentation.authentication.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import app.igormatos.botaprarodar.common.BprErrorType
+import app.igormatos.botaprarodar.data.model.error.UserAdminErrorException
+import app.igormatos.botaprarodar.data.repository.AdminRepository
+import app.igormatos.botaprarodar.presentation.authentication.Validator
+import app.igormatos.botaprarodar.presentation.authentication.viewmodel.EmailValidationState.*
+import kotlinx.coroutines.launch
 
-class EmailValidationViewModel : ViewModel() {
-    private val _state = MutableLiveData<EmailValidationState>()
-    val state: LiveData<EmailValidationState>
-        get() = _state
+class EmailValidationViewModel(
+    private val adminRepository: AdminRepository,
+    private val emailValidator: Validator<String>
+) : ViewModel() {
+    private val _viewState = MutableLiveData<EmailValidationState>()
+    val viewState: LiveData<EmailValidationState>
+        get() = _viewState
 
-    val progressBarEnabled: Boolean = state.value is EmailValidationState.Loading
+    val progressBarVisible: LiveData<Boolean>
+        get() = Transformations.map(viewState) {
+            it is SendLoading
+        }
 
+    val emailField = MutableLiveData<String>().apply {
+        if (viewState.value is InitialState) {
+            this.value = ""
+        }
+    }
+
+    val nextButtonEnabled: LiveData<Boolean>
+        get() = Transformations.map(emailField) {
+            emailValidator.validate(it)
+        }
+
+    init {
+        _viewState.value = InitialState
+    }
+
+    fun sendForm() {
+        viewModelScope.launch {
+            _viewState.value = SendLoading
+            try {
+                emailField.value?.let {
+                    if (emailValidator.validate(it)) {
+                        val result = adminRepository.isAdminRegistered(it)
+                        _viewState.value = SendSuccess(result)
+                    }
+                }
+            } catch (e: UserAdminErrorException.AdminNetwork) {
+                _viewState.value = SendError(type = BprErrorType.NETWORK)
+            } catch (e: Exception) {
+                _viewState.value = SendError(type = BprErrorType.UNKNOWN)
+            }finally {
+                _viewState.postValue(Completed)
+            }
+        }
+    }
 }
