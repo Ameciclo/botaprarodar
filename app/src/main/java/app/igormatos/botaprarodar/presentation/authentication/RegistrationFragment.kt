cac099b01eac32d480c6fe4b14ab46392eccfff3
IsabellaLima <bella.liamm@gmail.com>
2021-01-14 17:42:57 -0300

    Updates view model and fragment with email and password validations and registration complete dialog

--- app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/RegistrationFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/RegistrationFragment.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import app.igormatos.botaprarodar.R
+import app.igormatos.botaprarodar.common.BprErrorType
+import app.igormatos.botaprarodar.databinding.DialogRegistrationSuccessBinding
 import app.igormatos.botaprarodar.databinding.FragmentRegistrationBinding
 import app.igormatos.botaprarodar.presentation.authentication.viewmodel.RegistrationState
 import app.igormatos.botaprarodar.presentation.authentication.viewmodel.RegistrationViewModel
 import com.brunotmgomes.ui.extensions.createLoading
-import com.brunotmgomes.ui.extensions.snackBarMaker
+import com.firebase.ui.auth.util.ui.BaselineTextInputLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
+const val MIN_PASSWORD_LENGTH = 6
 
 class RegistrationFragment: Fragment() {
     private lateinit var binding : FragmentRegistrationBinding
     private val registrationViewModel : RegistrationViewModel by viewModel()
     private lateinit var loadingDialog : AlertDialog
+    private var validator = EmailValidator()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +41,6 @@ class RegistrationFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
         bindViewModel()
     }
 
@@ -44,20 +53,76 @@ class RegistrationFragment: Fragment() {
             }
         })
         registrationViewModel.registrationState.observe(viewLifecycleOwner, Observer {
-            when(it) {
-                is RegistrationState.Success -> snackBarMaker(getString(R.string.promt_email_confirmation_title), binding.container)
-                is RegistrationState.Error -> snackBarMaker(getString(R.string.promt_registration_error), binding.container)
-                is RegistrationState.InvalidEmail -> binding.emailLayout.error = getString(R.string.email_format_warning)
+            when (it) {
+                is RegistrationState.SendSuccess -> onAdminRegistered()
+                is RegistrationState.SendError -> {
+                    val errorMessage = createErrorMessage(it.type)
+                    showErrorMessage(errorMessage)
+                }
+                else -> {
+                    // Do Nothing
+                }
             }
         })
+        registrationViewModel.emailValid.observe(viewLifecycleOwner, Observer {
+            handleEmailErrorMessage(it)
+        })
+        registrationViewModel.passwordValid.observe(viewLifecycleOwner, Observer {
+            handlePasswordErrorMessage(it)
+        })
         binding.save.setOnClickListener {
-            registrationViewModel.createAccount(binding.email.text.toString().trim(), binding.password.text.toString().trim())
+            registrationViewModel.createAccount(
+                binding.email.text.toString().trim(),
+                binding.password.text.toString().trim()
+            )
         }
     }
 
-    private fun verifyPasswordLength() {
-
+    private fun onAdminRegistered() {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        val dialogBinding = DialogRegistrationSuccessBinding.inflate(layoutInflater)
+        dialogBuilder.setView(dialogBinding.root)
+        val dialog = dialogBuilder.create()
+        dialog.show()
+        dialogBinding.loginButton.setOnClickListener {
+            dialog.hide()
+            findNavController().navigate(R.id.action_registrationFragment_to_loginActivity)
+        }
     }
+
+    private fun handlePasswordErrorMessage(isValidInput: Boolean) {
+        binding.passwordLayout.apply {
+            if (isValidInput) {
+                isErrorEnabled = false
+            } else {
+                error = getString(R.string.promt_password_min_length_warning)
+            }
+        }
+    }
+
+    private fun handleEmailErrorMessage(isValidInput: Boolean) {
+        binding.emailLayout.apply {
+            if (isValidInput) {
+                isErrorEnabled = false
+            } else {
+                error = getString(R.string.email_format_warning)
+            }
+        }
+    }
+
+    private fun showErrorMessage(@StringRes messageId: Int) {
+        Snackbar.make(
+            binding.container,
+            messageId,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun createErrorMessage(errorType: BprErrorType): Int =
+        when (errorType) {
+            BprErrorType.NETWORK -> R.string.network_error_message
+            BprErrorType.UNKNOWN -> R.string.login_error
+        }
 
     private fun showLoadingDialog() {
         loadingDialog.show()
