b023197020298eb0fa60158fa37a4ff87b3aec58
Akme-re <akmere.almeida@thoughtworks.com>
2021-01-15 11:23:01 -0300

    add isValidPassword extension

--- toolkit/src/main/java/com/brunotmgomes/ui/extensions/CharSequenceExtension.kt
+++ toolkit/src/main/java/com/brunotmgomes/ui/extensions/CharSequenceExtension.kt
@@ -3,3 +3,4 @@ package com.brunotmgomes.ui.extensions
 import android.util.Patterns
 
 fun CharSequence?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+fun CharSequence?.isValidPassword() = !isNullOrEmpty() && this.length >= 6
