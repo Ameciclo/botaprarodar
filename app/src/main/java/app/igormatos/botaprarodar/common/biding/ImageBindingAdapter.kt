7c420865875d5f9946dbae21291b26055012bfa3
Wilquer Torres de Lima <wilquer.lima@MacBook-Pro-de-Wilquer.local>
2021-01-22 16:10:23 -0300

    put error state to bicycle photo

--- app/src/main/java/app/igormatos/botaprarodar/common/biding/ImageBindingAdapter.kt
+++ app/src/main/java/app/igormatos/botaprarodar/common/biding/ImageBindingAdapter.kt
@@ -2,16 +2,21 @@ package app.igormatos.botaprarodar.common.biding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import app.igormatos.botaprarodar.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 
 
 @BindingAdapter("app:imagePathOrUrl")
 fun setImagePathOrUrl(imageView: ImageView, imagePathOrUrl: String) {
-    if(imagePathOrUrl.isNotBlank()) {
-        Glide.with(imageView)
-            .load(imagePathOrUrl)
-            .apply(RequestOptions.fitCenterTransform())
-            .into(imageView)
-    }
+    Glide.with(imageView)
+        .load(imagePathOrUrl)
+        .apply(getRequestOptions())
+        .into(imageView)
 }
+
+private fun getRequestOptions() = RequestOptions()
+    .fitCenter()
+    .error(R.drawable.ic_add_a_photo)
+    .diskCacheStrategy(DiskCacheStrategy.ALL)
