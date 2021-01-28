4236b3898ee66b34a77e69198110d917d48f6349
Akme-re <akmere.almeida@thoughtworks.com>
2021-01-28 14:41:42 -0300

    rename test class and imports cleaning

--- app/src/androidTest/java/app/igormatos/botaprarodar/authentication/AdminSignIn.kt
+++ app/src/androidTest/java/app/igormatos/botaprarodar/authentication/AdminSignIn.kt
@@ -2,26 +2,21 @@ package app.igormatos.botaprarodar.authentication
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.intent.Intents
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import app.igormatos.botaprarodar.data.repository.AdminRemoteDataSource
 import app.igormatos.botaprarodar.presentation.authentication.AuthenticationActivity
-import app.igormatos.botaprarodar.presentation.main.MainActivity
 import com.google.common.truth.Truth.assertThat
-import io.mockk.coEvery
-import io.mockk.mockk
-import org.junit.*
+import org.junit.Before
+import org.junit.FixMethodOrder
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
-import org.koin.core.context.stopKoin
 
 
 @RunWith(AndroidJUnit4::class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @LargeTest
-class SignIn {
+class AdminSignIn {
     private lateinit var scenario: ActivityScenario<AuthenticationActivity>
 
     @Before
