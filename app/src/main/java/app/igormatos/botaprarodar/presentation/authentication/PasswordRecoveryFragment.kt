0ef1b13465aab73feb5a446f9c80fbe4577ab9e7
Akme-re <akmere.almeida@thoughtworks.com>
2021-02-01 14:16:37 -0300

    refactor toast to snackbar with different colors

--- app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/PasswordRecoveryFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/authentication/PasswordRecoveryFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import app.igormatos.botaprarodar.R
 import app.igormatos.botaprarodar.common.BprErrorType
 import app.igormatos.botaprarodar.databinding.FragmentPasswordRecoveryBinding
 import app.igormatos.botaprarodar.presentation.authentication.viewmodel.PasswordRecoveryViewModel
 import app.igormatos.botaprarodar.presentation.authentication.viewmodel.SendPasswordRecoveryViewState
-import com.brunotmgomes.ui.extensions.createLoading
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel as koinViewModel
 
@@ -33,7 +33,10 @@ class PasswordRecoveryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.viewmodel?.viewState?.observe(viewLifecycleOwner, { viewState ->
             when (viewState) {
-                SendPasswordRecoveryViewState.SendSuccess -> showSuccessToast()
+                SendPasswordRecoveryViewState.SendSuccess -> {
+                    showSuccessSnackbar()
+                    findNavController().navigateUp()
+                }
                 is SendPasswordRecoveryViewState.SendError -> showErrorSnackbar(viewState.type)
                 else -> {
                     // nothing to do
@@ -47,14 +50,21 @@ class PasswordRecoveryFragment : Fragment() {
             BprErrorType.NETWORK -> R.string.network_error_message
             else -> R.string.reset_password_email_sent_error
         }
-        Snackbar.make(binding.root, getString(errorMessageId), Snackbar.LENGTH_SHORT).show()
+        val color = getColor(requireContext(), R.color.red)
+        createSnackbar(errorMessageId, color).show()
     }
 
-    private fun showSuccessToast() {
-        Toast.makeText(
-            requireContext(),
-            getString(R.string.reset_password_email_sent),
-            Toast.LENGTH_SHORT
-        ).show()
+    private fun showSuccessSnackbar() {
+        val color = getColor(requireContext(), R.color.green)
+        createSnackbar(R.string.reset_password_email_sent, color).show()
+    }
+
+    private fun createSnackbar(errorMessageId: Int, color: Int): Snackbar {
+        return Snackbar
+            .make(
+                binding.root,
+                getString(errorMessageId),
+                Snackbar.LENGTH_SHORT
+            ).apply { setBackgroundTint(color) }
     }
 }
