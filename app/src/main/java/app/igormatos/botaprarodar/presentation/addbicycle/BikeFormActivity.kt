7e77be5ca62faf5506cc1f8f7fc7bac684740fe4
Akme-re <akmere.almeida@thoughtworks.com>
2021-02-01 14:11:39 -0300

    optimize imports

--- app/src/main/java/app/igormatos/botaprarodar/presentation/addbicycle/BikeFormActivity.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/addbicycle/BikeFormActivity.kt
@@ -4,20 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import app.igormatos.botaprarodar.R
 import app.igormatos.botaprarodar.common.BikeFormStatus
-import app.igormatos.botaprarodar.domain.model.Bike
-import app.igormatos.botaprarodar.data.network.firebase.FirebaseHelper
 import app.igormatos.botaprarodar.databinding.ActivityBikeFormBinding
 import com.brunotmgomes.ui.extensions.REQUEST_PHOTO
-import com.brunotmgomes.ui.extensions.showLoadingDialog
 import com.brunotmgomes.ui.extensions.takePictureIntent
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import org.koin.androidx.viewmodel.ext.android.viewModel as koinViewModel
 
 val BIKE_EXTRA = "Bike_extra"
@@ -30,7 +24,7 @@ class BikeFormActivity : AppCompatActivity() {
     private val formViewModel: BikeFormViewModel by koinViewModel()
 
     private val binding: ActivityBikeFormBinding by lazy {
-        DataBindingUtil.setContentView<ActivityBikeFormBinding>(this, R.layout.activity_bike_form)
+        DataBindingUtil.setContentView(this, R.layout.activity_bike_form)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
