54b2fd0f7ef8618903daa5e769629289cfe7571f
Akme-re <akmere.almeida@thoughtworks.com>
2021-02-01 14:03:16 -0300

    refactor Home tests

--- app/src/androidTest/java/app/igormatos/botaprarodar/accessapphome/AccessHomeTest.kt
+++ app/src/androidTest/java/app/igormatos/botaprarodar/accessapphome/AccessHomeTest.kt
@@ -1,46 +1,35 @@
 package app.igormatos.botaprarodar.accessapphome
 
-import android.content.Context
-import android.content.Intent
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import app.igormatos.botaprarodar.login.login
-import app.igormatos.botaprarodar.presentation.login.LoginActivity
-import com.google.firebase.FirebaseApp
+import app.igormatos.botaprarodar.authentication.login
+import app.igormatos.botaprarodar.presentation.welcome.WelcomeActivity
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AccessHomeTest {
-
-    private val intent =
-        Intent(ApplicationProvider.getApplicationContext(), LoginActivity::class.java)
-    private lateinit var loginActivity: ActivityScenario<LoginActivity>
-    private lateinit var mContext: Context
+    private lateinit var scenario: ActivityScenario<WelcomeActivity>
 
     @Before
-    fun setUp() {
-        loginActivity = launchActivity(intent)
-        loginActivity.onActivity {
-            FirebaseApp.initializeApp(it)
-            mContext = it
-        }
+    fun setup() {
+        scenario = launchActivity()
     }
 
     @Test
     fun access_home_as_admin() {
         login {
-            doLogin("brunotmg@gmail.com", "abcd1234")
-            sleep(3000)
+            initAuthentication()
+            successfulLoginSteps("brunotmg@gmail.com", "abcd1234")
         }
         selectCommunity {
+            showCommunityList()
             selectAnyCommunity()
-            sleep(3000)
         } verify {
-            checkIfIsHome()
+            assertThat(checkIfIsHome()).isTrue()
         }
     }
 }
