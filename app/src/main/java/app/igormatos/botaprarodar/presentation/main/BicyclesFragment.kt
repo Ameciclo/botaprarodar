834b9ea215fa7986ef88404bfa4aaae6ec79a089
Thiago Matheus <thiagomatheus@MacBook-Pro-de-Thiago.local>
2021-02-18 13:26:31 -0300

    Finalize Bikes layout and add data binding

--- app/src/main/java/app/igormatos/botaprarodar/presentation/main/BicyclesFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/main/BicyclesFragment.kt
@@ -6,30 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import app.igormatos.botaprarodar.R
-import app.igormatos.botaprarodar.domain.model.Bike
 import app.igormatos.botaprarodar.data.local.SharedPreferencesModule
-import app.igormatos.botaprarodar.data.network.firebase.FirebaseHelper
 import app.igormatos.botaprarodar.data.network.RequestListener
+import app.igormatos.botaprarodar.data.network.firebase.FirebaseHelper
+import app.igormatos.botaprarodar.data.network.firebase.FirebaseHelperModule
+import app.igormatos.botaprarodar.databinding.FragmentBikesBinding
+import app.igormatos.botaprarodar.domain.model.Bike
 import app.igormatos.botaprarodar.presentation.BicycleAdapterListener
 import app.igormatos.botaprarodar.presentation.BicyclesAdapter
 import app.igormatos.botaprarodar.presentation.addbicycle.BikeFormActivity
-import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_bikes.*
 import org.koin.android.ext.android.inject
 
 class BicyclesFragment : Fragment(), BicycleAdapterListener {
 
     lateinit var bicycleAdapter: BicyclesAdapter
     private val preferencesModule: SharedPreferencesModule by inject()
+    private val firebaseHelper: FirebaseHelperModule by inject()
+
+    private lateinit var binding: FragmentBikesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_list, container, false)
-        return rootView
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bikes, container, false)
+        return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,16 +47,16 @@ class BicyclesFragment : Fragment(), BicycleAdapterListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        addItemFab.setOnClickListener {
+        binding.btnRegisterBikes.setOnClickListener {
             val intent = Intent(it.context, BikeFormActivity::class.java)
             startActivity(intent)
         }
 
-        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
-        recyclerView.adapter = bicycleAdapter
+        binding.rvBikes.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
+        binding.rvBikes.adapter = bicycleAdapter
 
         val joinedCommunityId = preferencesModule.getJoinedCommunity().id!!
-        FirebaseHelper.getBicycles(joinedCommunityId, listener = object : RequestListener<Bike> {
+        firebaseHelper.getBicycles(joinedCommunityId, listener = object : RequestListener<Bike> {
             override fun onChildChanged(result: Bike) {
                 bicycleAdapter.updateItem(result)
             }
@@ -70,7 +76,7 @@ class BicyclesFragment : Fragment(), BicycleAdapterListener {
         Toast.makeText(requireContext(), bike.name, Toast.LENGTH_SHORT).show()
     }
 
-    override fun onBicycleLongClicked(bike: Bike) : Boolean {
+    override fun onBicycleLongClicked(bike: Bike): Boolean {
         Toast.makeText(requireContext(), bike.name, Toast.LENGTH_SHORT).show()
         return true
     }
---
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/main/BikesViewModel.kt
@@ -0,0 +1,6 @@
+package app.igormatos.botaprarodar.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class BikesViewModel : ViewModel() {
+}
