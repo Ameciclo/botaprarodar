ddff2ef0506e315be68a1c4964226d8db9e8bac4
Akme-re <akmere.almeida@thoughtworks.com>
2021-03-04 14:01:39 -0300

    add AdminDataSource interface

--- app/src/main/java/app/igormatos/botaprarodar/data/repository/AdminDataSource.kt
+++ app/src/main/java/app/igormatos/botaprarodar/data/repository/AdminDataSource.kt
@@ -1,5 +1,15 @@
 package app.igormatos.botaprarodar.data.repository
 
+import androidx.annotation.VisibleForTesting
+import app.igormatos.botaprarodar.data.model.Admin
+
 interface AdminDataSource {
-    suspend fun createAdmin()
+    suspend fun createAdmin(
+        email: String,
+        password: String
+    ): Admin?
+    @VisibleForTesting
+    suspend fun authenticateAdmin(email: String, password: String): Admin?
+    suspend fun sendPasswordRecoverEmail(email: String)
+    suspend fun isAdminRegistered(email: String): Boolean
 }
