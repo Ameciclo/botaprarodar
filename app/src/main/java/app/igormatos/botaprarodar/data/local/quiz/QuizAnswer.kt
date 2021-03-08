4d80055e7b425fe4d257b36360b96db5de3a330b
Akme-re <akmere.almeida@thoughtworks.com>
2021-03-08 17:37:33 -0300

    add quiz builder structure

---
+++ app/src/main/java/app/igormatos/botaprarodar/data/local/quiz/QuizAnswer.kt
@@ -0,0 +1,44 @@
+package app.igormatos.botaprarodar.data.local.quiz
+
+data class QuizAnswer(val value: Any, private val quizName: QuizAnswerName)
+
+interface QuizAnswerName{
+    fun getQuizName(): String
+}
+enum class WithdrawQuizQuizAnswerName(name: String): QuizAnswerName{
+    RESPOSTA_1("resposta 1") {
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_2("resposta 2") {
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_3("resposta 3"){
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_4("resposta 4"){
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_5("resposta 5"){
+        override fun getQuizName(): String = name
+    }
+}
+enum class ReturnQuizQuizAnswerName(name: String): QuizAnswerName{
+    RESPOSTA_1("resposta 1") {
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_2("resposta 2") {
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_3("resposta 3"){
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_4("resposta 4"){
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_5("resposta 5"){
+        override fun getQuizName(): String = name
+    },
+    RESPOSTA_6("resposta 6"){
+        override fun getQuizName(): String = name
+    }
+}
