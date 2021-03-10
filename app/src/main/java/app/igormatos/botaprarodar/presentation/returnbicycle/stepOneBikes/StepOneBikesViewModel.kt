d6cc036685993fcdd6471f5ef3634776220d23e6
Thiago Matheus <thiagomatheus@MacBook-Pro-de-Thiago.local>
2021-03-10 10:43:30 -0300

    Create navigateToNext on StepOneViewModel

--- app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/stepOneBikes/StepOneBikesViewModel.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/stepOneBikes/StepOneBikesViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.igormatos.botaprarodar.common.enumType.StepConfigType
 import app.igormatos.botaprarodar.data.repository.BikeRepository
 import app.igormatos.botaprarodar.domain.model.Bike
 import app.igormatos.botaprarodar.presentation.returnbicycle.StepperAdapter
@@ -13,13 +14,21 @@ import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class StepOneBikesViewModel(
-    val stepperAdapter: StepperAdapter.ReturnStepper,
+    private val stepperAdapter: StepperAdapter.ReturnStepper,
     private val stepOneBikesUseCase: StepOneBikesUseCase
 ) : ViewModel() {
 
     private val _bikesAvailableToReturn = MutableLiveData<SimpleResult<List<Bike>>>()
     val bikesAvailableToReturn: LiveData<SimpleResult<List<Bike>>>
         get() = _bikesAvailableToReturn
+
+    fun setInitialStep() {
+        stepperAdapter.setCurrentStep(StepConfigType.SELECT_BIKE)
+    }
+
+    fun navigateToNextStep() {
+        stepperAdapter.navigateToNext()
+    }
 
     fun getBikesInUseToReturn(communityId: String) {
         viewModelScope.launch {
