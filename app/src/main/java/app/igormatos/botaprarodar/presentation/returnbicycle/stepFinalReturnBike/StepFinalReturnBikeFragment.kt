6f2f4d91a03e42ef0dc9189819ecac233032d38a
Thiago Matheus <thiagomatheus@MacBook-Pro-de-Thiago.local>
2021-03-17 11:33:50 -0300

    Add conditional if devolutions list is null and fix the bike created_date when add a new devolution

--- app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/stepFinalReturnBike/StepFinalReturnBikeFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/stepFinalReturnBike/StepFinalReturnBikeFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import app.igormatos.botaprarodar.R
 import app.igormatos.botaprarodar.databinding.FragmentStepFinalReturnBikeBinding
 import app.igormatos.botaprarodar.presentation.returnbicycle.stepOneReturnBike.StepOneReturnBikeViewModel
+import com.brunotmgomes.ui.SimpleResult
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class StepFinalReturnBikeFragment() : Fragment() {
+class StepFinalReturnBikeFragment : Fragment() {
 
     private lateinit var binding: FragmentStepFinalReturnBikeBinding
     private val viewModel: StepFinalReturnBikeViewModel by viewModel()
@@ -35,6 +38,20 @@ class StepFinalReturnBikeFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initObserver()
+    }
+
+    private fun initObserver() {
+        viewModel.state.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is SimpleResult.Success -> {
+                    Toast.makeText(requireContext(), "Sucesso", Toast.LENGTH_SHORT).show()
+                }
+                is SimpleResult.Error -> {
+                    Toast.makeText(requireContext(), "ERRO", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
 }
