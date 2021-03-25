be245a3d6d6f021d1a30e2e485d2481d2ae7acee
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-03-25 14:27:52 -0300

    Partial implementation of UserActivity

---
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/user/UserActivity.kt
@@ -0,0 +1,13 @@
+package app.igormatos.botaprarodar.presentation.user
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.igormatos.botaprarodar.R
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
