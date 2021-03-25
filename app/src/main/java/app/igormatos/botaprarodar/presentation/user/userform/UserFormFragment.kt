e9f0ac8c800bdb5c10390fbbf93c141355566d3e
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-03-25 14:11:24 -0300

    Partial extraction of UserFormFragment

--- app/src/main/java/app/igormatos/botaprarodar/presentation/user/userform/UserFormFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/user/userform/UserFormFragment.kt
@@ -4,19 +4,20 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import app.igormatos.botaprarodar.R
 import app.igormatos.botaprarodar.common.ViewModelStatus
 import app.igormatos.botaprarodar.common.components.CustomDialog
 import app.igormatos.botaprarodar.common.components.CustomDialog.Companion.TAG
-import app.igormatos.botaprarodar.databinding.ActivityAddUserBinding
+import app.igormatos.botaprarodar.databinding.FragmentUserFormBinding
 import app.igormatos.botaprarodar.domain.model.CustomDialogModel
 import app.igormatos.botaprarodar.domain.model.User
 import com.brunotmgomes.ui.extensions.*
@@ -24,16 +25,14 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.jetbrains.anko.image
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class UserFormActivity : AppCompatActivity() {
-
-    private val binding: ActivityAddUserBinding by lazy {
-        DataBindingUtil.setContentView<ActivityAddUserBinding>(this, R.layout.activity_add_user)
-    }
+class UserFormFragment : Fragment() {
 
     private val userFormViewModel: UserFormViewModel by viewModel()
     private var mCurrentPhotoPath = ""
     private var currentPhotoId = 0
     private lateinit var loadingDialog: AlertDialog
+
+    private lateinit var binding: FragmentUserFormBinding
 
     companion object {
         private const val REQUEST_PROFILE_PHOTO = 1
@@ -43,27 +42,35 @@ class UserFormActivity : AppCompatActivity() {
         const val USER_EXTRA = "USER_EXTRA"
 
         fun setupActivity(context: Context, user: User?): Intent {
-            val intent = Intent(context, UserFormActivity::class.java)
+            val intent = Intent(context, UserFormFragment::class.java)
             intent.putExtra(USER_EXTRA, user)
             return intent
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_user)
-
-        binding.lifecycleOwner = this
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUserFormBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = userFormViewModel
-        loadingDialog = createLoading(R.layout.loading_dialog_animation)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadingDialog = requireContext().createLoading(R.layout.loading_dialog_animation)
         setupListeners()
         setupViewModelStatus()
         checkEditMode()
     }
 
     private fun checkEditMode() {
-        if (intent.extras != null) {
-            val userExtra = intent.extras?.getParcelable<User>(USER_EXTRA)
+        if (arguments != null) {
+            val userExtra = arguments?.getParcelable<User>(USER_EXTRA)
             setValuesToEditUser(userExtra)
             setImageDescriptionsToGone()
             setImageEditDescriptionsToVisible()
@@ -89,7 +96,7 @@ class UserFormActivity : AppCompatActivity() {
     }
 
     private fun setupViewModelStatus() {
-        binding.viewModel?.status?.observe(this, Observer {
+        binding.viewModel?.status?.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is ViewModelStatus.Success -> {
                     loadingDialog.dismiss()
@@ -97,16 +104,17 @@ class UserFormActivity : AppCompatActivity() {
                         "isEditModeAvailable",
                         userFormViewModel.isEditableAvailable
                     )
-                    setResult(RESULT_OK, intent)
-                    finish()
+                    // TODO
+                    //setResult(RESULT_OK, intent)
+                    //finish()
                 }
                 is ViewModelStatus.Loading -> {
-                    window.decorView.hideKeyboard()
+                    requireActivity().window.decorView.hideKeyboard()
                     loadingDialog.show()
                 }
                 is ViewModelStatus.Error -> {
                     snackBarMaker(it.message, binding.scrollContainer).apply {
-                        setBackgroundTint(ContextCompat.getColor(applicationContext, R.color.red))
+                        setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
                         show()
                     }
                     loadingDialog.dismiss()
@@ -114,7 +122,7 @@ class UserFormActivity : AppCompatActivity() {
             }
         })
 
-        binding.viewModel?.lgpd?.observe(this, Observer {
+        binding.viewModel?.lgpd?.observe(viewLifecycleOwner, Observer {
             if (binding.viewModel?.isEditableAvailable == true) {
                 binding.viewModel?.registerUser()
             } else if (it) {
@@ -149,9 +157,10 @@ class UserFormActivity : AppCompatActivity() {
     }
 
     private fun dispatchTakePictureIntent(code: Int) {
-        takePictureIntent(code) { path ->
-            mCurrentPhotoPath = path
-        }
+        // TODO
+        //takePictureIntent(code) { path ->
+        //    mCurrentPhotoPath = path
+        //}
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -171,11 +180,11 @@ class UserFormActivity : AppCompatActivity() {
         val tipLayout = layoutInflater.inflate(R.layout.dialog_tip, null)
 
         tipLayout.findViewById<ImageView>(R.id.tipImage).image =
-            ContextCompat.getDrawable(this, image)
+            ContextCompat.getDrawable(requireContext(), image)
         tipLayout.findViewById<TextView>(R.id.tipTitle).text = title
         tipLayout.findViewById<TextView>(R.id.tipSubtitle).text = subtitle
 
-        MaterialAlertDialogBuilder(this)
+        MaterialAlertDialogBuilder(requireContext())
             .setView(tipLayout)
             .setPositiveButton("Tirar foto!") { _, _ ->
                 click(true)
@@ -237,6 +246,6 @@ class UserFormActivity : AppCompatActivity() {
             }
         )
 
-        CustomDialog.newInstance(dialogModel).show(supportFragmentManager, TAG)
+        CustomDialog.newInstance(dialogModel).show(childFragmentManager, TAG)
     }
 }
