e02ebaad1499b3767fd3c4207c8f0ef8ee3d856d
Thiago Matheus <thiagomatheus@MacBook-Pro-de-Thiago.local>
2021-03-31 12:27:47 -0300

    Fix UserFormViewModelTest

---
+++ app/src/test/java/app/igormatos/botaprarodar/presentation/user/userquiz/UserQuizViewModelTest.kt
@@ -0,0 +1,54 @@
+package app.igormatos.botaprarodar.presentation.user.userquiz
+
+class UserQuizViewModelTest {
+
+    //    @Test
+//    fun `when 'registerUser' should return success`() {
+//        formViewModel.userDocument.value = "1"
+//        val userFake = slot<User>()
+//        coEvery {
+//            userUseCase.addUser(capture(userFake))
+//        } returns userSimpleSuccess
+//
+//        formViewModel.registerUser()
+//        assertTrue(formViewModel.status.value is ViewModelStatus.Success)
+//    }
+//
+//    @Test
+//    fun `when 'registerUser' should return error`() {
+//        formViewModel.userDocument.value = "1"
+//        val userFake = slot<User>()
+//        coEvery {
+//            userUseCase.addUser(capture(userFake))
+//        } returns SimpleResult.Error(Exception())
+//
+//        formViewModel.registerUser()
+//        assertTrue(formViewModel.status.value is ViewModelStatus.Error)
+//    }
+
+    //    @Test
+//    fun `when 'registerUser' and 'isEditableAvailable' is true should return success`() {
+//        formViewModel.userDocument.value = "1"
+//        formViewModel.isEditableAvailable = true
+//        val userFake = slot<User>()
+//        coEvery {
+//            userUseCase.startUpdateUser(capture(userFake))
+//        } returns userSimpleSuccess
+//
+//        formViewModel.registerUser()
+//        assertTrue(formViewModel.status.value is ViewModelStatus.Success)
+//    }
+//
+//    @Test
+//    fun `when 'registerUser' and 'IsEditableAvailable' is true should return error`() {
+//        formViewModel.userDocument.value = "1"
+//        formViewModel.isEditableAvailable = true
+//        val userFake = slot<User>()
+//        coEvery {
+//            userUseCase.startUpdateUser(capture(userFake))
+//        } returns SimpleResult.Error(Exception())
+//
+//        formViewModel.registerUser()
+//        assertTrue(formViewModel.status.value is ViewModelStatus.Error)
+//    }
+}
