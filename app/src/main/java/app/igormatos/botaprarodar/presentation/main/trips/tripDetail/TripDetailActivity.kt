56f1a2f513de83e72bc29b875528f4151875df1d
Thiago Matheus <thiagomatheus@MacBook-Pro-de-Thiago.local>
2021-05-13 15:36:25 -0300

    Create layout for TripDetail

---
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/main/trips/tripDetail/TripDetailActivity.kt
@@ -0,0 +1,11 @@
+package app.igormatos.botaprarodar.presentation.main.trips.tripDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TripDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_detail)
+    }
+}
