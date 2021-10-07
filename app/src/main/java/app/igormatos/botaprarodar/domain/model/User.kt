f5854962c2554d8ad3e304b20c6d4f649d5075cb
andriuswmdsilva <awmdsilva@outlook.com>
2021-10-07 15:49:11 -0300

    feature: Adjusted ID when creates new user

--- app/src/main/java/app/igormatos/botaprarodar/domain/model/User.kt
+++ app/src/main/java/app/igormatos/botaprarodar/domain/model/User.kt
@@ -66,10 +66,8 @@ data class User(
     }
 
     override fun title(): String {
-        val nameCapitalized =
-            name?.split(" ")?.take(2)?.map { it.capitalize() }?.joinToString(separator = " ") { it }
-
-        return nameCapitalized ?: "erro_01"
+        val names = name?.split(" ")
+        return names?.first()?.capitalize().orEmpty() + " " + names?.last()?.capitalize().orEmpty()
     }
 
     override fun iconPath(): String {
