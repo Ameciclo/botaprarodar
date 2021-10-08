2d99ab23193dbc0dba448db8e8c6aaedac68c269
andriuswmdsilva <awmdsilva@outlook.com>
2021-10-08 05:27:42 -0300

    feature: fix test delete local file to use tempfile and fix FirebaseHelperRepositoryTest test

--- app/src/test/java/app/igormatos/botaprarodar/data/repository/FirebaseHelperRepositoryTest.kt
+++ app/src/test/java/app/igormatos/botaprarodar/data/repository/FirebaseHelperRepositoryTest.kt
@@ -1,18 +1,20 @@
 package app.igormatos.botaprarodar.data.repository
 
 
-import app.igormatos.botaprarodar.utils.validUser
 import com.brunotmgomes.ui.SimpleResult
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GetTokenResult
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import io.mockk.MockKAnnotations.init
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
+import io.mockk.justRun
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-
 
 class FirebaseHelperRepositoryTest {
     private lateinit var firebaseHelperRepository: FirebaseHelperRepository
@@ -22,7 +24,6 @@ class FirebaseHelperRepositoryTest {
 
     @MockK
     private lateinit var storageReference: StorageReference
-
 
     @Before
     fun setUp() {
@@ -36,11 +37,12 @@ class FirebaseHelperRepositoryTest {
     fun `When deleteImageResource then should return a SimpleResult Success`(): Unit = runBlocking {
         val path = "imagePath.jpg"
 
-        coEvery { firebaseStorage.reference.child(path) } returns storageReference
         coEvery { firebaseStorage.getReferenceFromUrl(path) } returns storageReference
 
-        coEvery { storageReference.delete() }
+        coEvery { storageReference.delete() } returns mockk<Task<Void>>()
+
         val response = firebaseHelperRepository.deleteImageResource(path)
+
         assertEquals(response, SimpleResult.Success(Unit))
     }
 
