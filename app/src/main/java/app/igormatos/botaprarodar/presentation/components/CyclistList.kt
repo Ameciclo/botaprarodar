a92664e809b5a9673c314fc798b5b4f0b1f502f5
Joelyson David Oliveira Sobreira <joelyson.david@live.com>
2021-12-15 14:41:01 -0300

    Layout: Created Cyclist List

---
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/components/CyclistList.kt
@@ -0,0 +1,37 @@
+package app.igormatos.botaprarodar.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import app.igormatos.botaprarodar.R
+import app.igormatos.botaprarodar.presentation.bikewithdraw.viewmodel.SelectUserViewModel
+import app.igormatos.botaprarodar.presentation.components.ui.theme.BotaprarodarTheme
+
+
+@Composable
+fun CyclistListComponent(vm: SelectUserViewModel = viewModel()) {
+    val cyclistList = vm.userList.observeAsState()
+    Box {
+        LazyColumn(contentPadding = PaddingValues(vertical = dimensionResource(id = R.dimen.padding_minimun))) {
+            if (cyclistList.value != null) {
+                items(cyclistList.value!!) { cyclist ->
+                    CardCyclist(user = cyclist)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    BotaprarodarTheme {
+        CyclistListComponent()
+    }
+}
