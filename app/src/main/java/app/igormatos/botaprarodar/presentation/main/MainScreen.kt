e0ad1baa41db64b67d24b7333a8fb5d06a00d991
Felipe Moreno Borges <felipe.b2014@gmail.com>
2021-12-27 11:20:22 -0300

    Layout: Adding drawer to new home

--- app/src/main/java/app/igormatos/botaprarodar/presentation/main/MainScreen.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/main/MainScreen.kt
@@ -1,25 +1,91 @@
 package app.igormatos.botaprarodar.presentation.main
 
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import app.igormatos.botaprarodar.data.local.SharedPreferencesModule
+import app.igormatos.botaprarodar.presentation.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @Composable
 fun MainScreen(homeUiState: HomeUiState) {
     val navController = rememberNavController()
-    Scaffold(bottomBar = { BottomBar(navHostController = navController)}) {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
+    Scaffold(
+        topBar = { TopBar(onClickMenuHandler = suspend { scaffoldState.drawerState.open() }) },
+        bottomBar = { BottomBar(navHostController = navController) },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            Text("Menu")
+        }
+    ) {
         BottomNavGraph(navHostController = navController, uiState = homeUiState)
+    }
+}
+
+@Composable
+fun TopBar(onClickMenuHandler: suspend () -> Unit) {
+    TopAppBar(
+        title = { Text("Bota Pra Rodar") },
+        navigationIcon = { MenuIcon(onClickMenuHandler) },
+        actions = { LogoutButton() },
+        backgroundColor = Color.White,
+        modifier = Modifier.border(
+            border = BorderStroke(0.dp, Color.Transparent),
+            shape = RoundedCornerShape(bottomStart = 4.dp, bottomEnd = 4.dp)
+        )
+    )
+}
+
+@Composable
+private fun LogoutButton() {
+    val context = LocalContext.current
+    IconButton(onClick = { logout(context = context) }) {
+        Icon(imageVector = Icons.Filled.Logout, contentDescription = "")
+    }
+}
+
+private fun logout(context: Context) {
+    FirebaseAuth.getInstance().signOut()
+
+    val preferencesModule = SharedPreferencesModule(context)
+    preferencesModule.clear()
+
+    val intent = Intent(context, LoginActivity::class.java)
+    context.startActivity(intent)
+}
+
+@Composable
+private fun MenuIcon(onClickMenuHandler: suspend () -> Unit) {
+    val scope = rememberCoroutineScope()
+    IconButton(onClick = {
+        scope.launch { onClickMenuHandler() }
+    }) {
+        Icon(imageVector = Icons.Filled.Menu, contentDescription = "")
     }
 }
 
@@ -71,6 +137,13 @@ fun RowScope.AddItem(
         selectedContentColor = Color.White,
         unselectedContentColor = Color(0xFF515151)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    val rememberScaffoldState = rememberScaffoldState()
+    TopBar(suspend { rememberScaffoldState.drawerState.close() })
 }
 
 @Preview
