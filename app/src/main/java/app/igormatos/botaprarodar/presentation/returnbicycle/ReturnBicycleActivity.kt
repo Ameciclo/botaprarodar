f9f0da379437cae1d8a51cbd978951683d49f030
Joelyson David <joelyson.david@live.com>
2022-01-24 17:03:30 -0300

    feature: shows list of bicycles on ReturnBicycleActivity
    Co-authored-by: alexandreti <alexix.dasilva@gmail.com>

--- app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/ReturnBicycleActivity.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/returnbicycle/ReturnBicycleActivity.kt
@@ -3,6 +3,7 @@ package app.igormatos.botaprarodar.presentation.returnbicycle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -17,9 +18,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.igormatos.botaprarodar.R
 import app.igormatos.botaprarodar.common.enumType.StepConfigType
+import app.igormatos.botaprarodar.domain.model.Bike
+import app.igormatos.botaprarodar.presentation.components.BikeList
 import app.igormatos.botaprarodar.presentation.components.ThreeStepper
 import app.igormatos.botaprarodar.presentation.components.button.BackButtom
+import app.igormatos.botaprarodar.presentation.components.ui.theme.ColorPallet
 import app.igormatos.botaprarodar.presentation.returnbicycle.ui.theme.BotaprarodarTheme
+import java.util.*
 
 class ReturnBicycleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +32,7 @@ class ReturnBicycleActivity : ComponentActivity() {
         setContent {
             BotaprarodarTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    ReturnBicyclePage()
+                    ReturnBicyclePage(bikes = emptyList(), handleClick = {})
                 }
             }
         }
@@ -36,7 +41,7 @@ class ReturnBicycleActivity : ComponentActivity() {
 
 
 @Composable
-fun ReturnBicyclePage() {
+fun ReturnBicyclePage(bikes: List<Bike>, handleClick: (Bike) -> Unit) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxWidth()) {
             BackButtom {}
@@ -54,7 +59,9 @@ fun ReturnBicyclePage() {
                     secondIcon = painterResource(id = R.drawable.ic_quiz)
                 )
             }
-            // TODO - Lista de bicicletas
+            Box(modifier = Modifier.background(ColorPallet.BackgroundGray)) {
+                BikeList(bikes = bikes, handleClick = handleClick)
+            }
         }
     }
 }
@@ -62,7 +69,37 @@ fun ReturnBicyclePage() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun ReturnBicycleActivityPreview() {
+    val listBikes = listOf(
+        Bike(
+            id = "",
+            isAvailable = true,
+            inUse = false,
+            name = "New Bicycle",
+            orderNumber = 1010,
+            serialNumber = "New Serial",
+            createdDate = Date().toString()
+        ),
+        Bike(
+            id = "",
+            isAvailable = true,
+            inUse = false,
+            name = "New Bicycle",
+            orderNumber = 1010,
+            serialNumber = "New Serial",
+            createdDate = Date().toString()
+        ),
+        Bike(
+            id = "",
+            isAvailable = true,
+            inUse = false,
+            name = "New Bicycle",
+            orderNumber = 1010,
+            serialNumber = "New Serial",
+            createdDate = Date().toString()
+        )
+    )
+
     BotaprarodarTheme {
-        ReturnBicyclePage()
+        ReturnBicyclePage(bikes = listBikes, handleClick = {})
     }
 }
