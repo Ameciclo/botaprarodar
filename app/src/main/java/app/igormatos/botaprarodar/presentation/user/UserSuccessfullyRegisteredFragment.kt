e3a06b4cd34bd12aa7b4e0bb0280aef3654eedcf
ramonmluz <rmluzz@gmail.com>
2022-08-25 15:34:15 -0300

    ramonmluz: Fix validation in Social Data Fragment that had been broken

--- app/src/main/java/app/igormatos/botaprarodar/presentation/user/UserSuccessfullyRegisteredFragment.kt
+++ app/src/main/java/app/igormatos/botaprarodar/presentation/user/UserSuccessfullyRegisteredFragment.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.fragment.app.Fragment
 import app.igormatos.botaprarodar.databinding.FragmentUserSuccessfullyRegisteredBinding
 import app.igormatos.botaprarodar.presentation.components.WithdrawStepperActivity
+import app.igormatos.botaprarodar.presentation.main.HomeActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class UserSuccessfullyRegisteredFragment : Fragment() {
 
     private lateinit var binding: FragmentUserSuccessfullyRegisteredBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,21 +26,36 @@ class UserSuccessfullyRegisteredFragment : Fragment() {
         return binding.root
     }
 
+    @ExperimentalComposeUiApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupGoHomeButtonListener()
         setupLendButtonListener()
     }
 
+    @ExperimentalComposeUiApi
     private fun setupGoHomeButtonListener() {
         binding.goHomeButton.setOnClickListener {
-            activity?.finish()
+            activity?.let {
+                val intent = HomeActivity.getStartIntent(it)
+                navigateToNextActivity(intent)
+            }
+        }
+
+    }
+
+    @ExperimentalCoroutinesApi
+    private fun setupLendButtonListener() {
+        binding.lendButton.setOnClickListener {
+            activity?.let {
+                val intent = WithdrawStepperActivity.startIntent(it)
+                navigateToNextActivity(intent)
+            }
         }
     }
 
-    private fun setupLendButtonListener() {
-        binding.lendButton.setOnClickListener {
-            context?.startActivity(Intent(context, WithdrawStepperActivity::class.java))
-            activity?.finish()
-        }
+    private fun navigateToNextActivity(intent: Intent) {
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        requireActivity().startActivity(intent)
+        activity?.finish()
     }
 }
