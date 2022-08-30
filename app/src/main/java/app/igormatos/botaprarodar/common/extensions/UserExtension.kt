af872ff2341598df73ae6a334ebf70ffeb361fa3
aplanos <alejandro.p@thoughtworks.com>
2022-08-30 11:06:55 -0300

    aplanos - order user list by name

--- app/src/main/java/app/igormatos/botaprarodar/common/extensions/UserExtension.kt
+++ app/src/main/java/app/igormatos/botaprarodar/common/extensions/UserExtension.kt
@@ -3,5 +3,8 @@ package app.igormatos.botaprarodar.common.extensions
 import app.igormatos.botaprarodar.domain.model.User
 
 fun List<User>.onlyAvailableUsers(): List<User> {
-    return this.filter { bike -> bike.isAvailable }
+    return this.filter { user -> user.isAvailable }
+
+}fun List<User>.sort(): List<User> {
+    return this.sortedBy { user -> user.name }
 }
--- app/src/main/java/app/igormatos/botaprarodar/domain/usecase/users/UsersUseCase.kt
+++ app/src/main/java/app/igormatos/botaprarodar/domain/usecase/users/UsersUseCase.kt
@@ -1,6 +1,7 @@
 package app.igormatos.botaprarodar.domain.usecase.users
 
 import app.igormatos.botaprarodar.common.extensions.onlyAvailableUsers
+import app.igormatos.botaprarodar.common.extensions.sort
 import app.igormatos.botaprarodar.data.repository.UserRepository
 import app.igormatos.botaprarodar.domain.model.User
 import com.brunotmgomes.ui.SimpleResult
@@ -12,7 +13,11 @@ class UsersUseCase(private val userRepository: UserRepository) {
     suspend fun getAvailableUsersByCommunityId(communityId: String): SimpleResult<List<User>> {
         return when(val result = userRepository.getUsersByCommunityId(communityId)) {
             is SimpleResult.Success -> {
-                SimpleResult.Success(result.data.values.toList().onlyAvailableUsers())
+                SimpleResult.Success(
+                    result.data.values.toList()
+                        .onlyAvailableUsers()
+                        .sort()
+                )
             }
             is SimpleResult.Error -> {
                 SimpleResult.Error(result.exception)
